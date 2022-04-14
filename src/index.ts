67d6a1b4796a24913ed8140019355a777726bf24
Midstackdev <midstackdev@gmail.com>
2022-04-14 00:17:16 +0000

    add express middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const PORT = 5000;
 
 const app: Application = express();
 
+app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message:
+      'Too many accounts created from this IP, please try again after an hour',
+  })
+);
+
+app.post('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello from post',
+    data: req.body,
+  });
+});
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World',
